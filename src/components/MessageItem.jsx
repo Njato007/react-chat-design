a9c5d1e752f44277fbc697827bb4970aea592253
Njato007 <njatotianafiononana@gmail.com>
2023-08-04 15:53:39 +0300

    close

--- src/components/MessageItem.jsx
+++ src/components/MessageItem.jsx
@@ -1,12 +1,12 @@
 import { useClickAway } from '@uidotdev/usehooks'
 import { motion } from 'framer-motion'
 import React, { useEffect, useState, useCallback } from 'react'
-import { BsPencil, BsReply, BsTrash } from 'react-icons/bs'
+import { BsEyeSlash, BsPencil, BsReply, BsTrash } from 'react-icons/bs'
 import { LuCopy } from 'react-icons/lu'
 import { PiShareFatLight } from 'react-icons/pi'
 import { useAxesStyle } from '../utils/tools'
 
-export const ContextMenu = ({ variant, x, y, closeContextMenu, onCopy, onTransfert, onReply, onDelete, OnUpdate }) => {
+export const ContextMenu = ({ variant, x, y, closeContextMenu, onCopy, onTransfert, onReply, onDelete, OnUpdate, onUnread }) => {
 
     const ref = useClickAway(() => {
         closeContextMenu();
@@ -15,7 +15,7 @@ export const ContextMenu = ({ variant, x, y, closeContextMenu, onCopy, onTransfe
     const axesStyle = useAxesStyle(x, y);
 
     return (
-        <motion.div ref={ref} className='absolute z-[1000]' style={axesStyle()}>
+        <motion.div ref={ref} className='fixed z-[1000]' style={axesStyle()}>
             <div className='flex flex-col text-sm bg-white shadow rounded' onClick={() => closeContextMenu()}>
                 <button className='context-menu-item' onClick={() => onCopy()}>
                     <LuCopy />
@@ -31,7 +31,7 @@ export const ContextMenu = ({ variant, x, y, closeContextMenu, onCopy, onTransfe
                     <PiShareFatLight />
                     Transférer
                 </button>
-                <button className='context-menu-item' onClick={() => onReply()}>
+                <button className='context-menu-item' onClick={() => onReply && onReply()}>
                     <BsReply />
                     Répondre
                 </button>
@@ -39,6 +39,12 @@ export const ContextMenu = ({ variant, x, y, closeContextMenu, onCopy, onTransfe
                     variant === 'sender' && <button className='context-menu-item' onClick={() => onDelete()}>
                         <BsTrash />
                         Supprimer
+                    </button>
+                }
+                {
+                    variant !== 'sender' && <button className='context-menu-item' onClick={() => onUnread && onUnread()}>
+                        <BsEyeSlash />
+                        Marquer comme non lu à partir d&apos;ici
                     </button>
                 }
             </div>
---
+++ src/components/ui/ScrollContainer.jsx
@@ -0,0 +1,112 @@
+import { useRef, useEffect, useState, useCallback } from "react";
+import { HiOutlineChevronDown } from "react-icons/hi";
+import { IoMdRefresh } from "react-icons/io";
+
+const ScrollContainer = ({ children, haveNewMessage, dragActive, onScrollTop, onReadMessage }) => {
+  const outerDiv = useRef(null);
+  const innerDiv = useRef(null);
+
+  const prevInnerDivHeight = useRef(null);
+
+  const [showNewMessages, setShowNewMessages] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [showScrollButton, setShowScrollButton] = useState(false);
+  
+  useEffect(() => {
+    setIsLoading(false)
+  }, []);
+
+  // scroll to bottom when loading is finished  
+  useEffect(() => {
+    if (!isLoading) {
+      const innerDivHeight = innerDiv.current.clientHeight;
+      outerDiv.current.scrollTo({
+        top: innerDivHeight,
+        behavior: "auto"
+      });
+      setIsLoading(false);
+    }
+  }, [isLoading]);
+  
+  // scroll to bottom when send message or new message comes in
+  // depends on scroll position
+  useEffect(() => {
+    const sh = Math.floor(outerDiv.current.scrollTop + outerDiv.current.clientHeight * 2);
+    if (sh > outerDiv.current.scrollHeight) {
+      // scroll>ToBottom
+      outerDiv.current.scrollTop = outerDiv.current.scrollHeight;
+    } else {
+      // Pusuh notification here
+      setShowNewMessages(true);
+    }
+  }, [haveNewMessage]);
+
+  const handleScrollButtonClick = useCallback(() => {
+    const innerDivHeight = innerDiv.current.clientHeight;
+
+    outerDiv.current.scrollTo({
+      top: innerDivHeight,
+      left: 0,
+      behavior: "smooth"
+    });
+
+    setShowScrollButton(false);
+  }, []);
+  
+  const handleScroll = (e) => {
+    let element = e.target;
+    if (element.scrollTop <= 80) {
+      return onScrollTop()
+    }
+    const outerDivHeight = outerDiv.current;
+    // show scroll to bottom button
+    const sh = Math.floor(outerDivHeight.scrollTop + outerDivHeight.clientHeight * 2);
+    setShowScrollButton(sh < outerDivHeight.scrollHeight);
+
+    const divs = element.querySelectorAll('[receiver]')
+    if (divs && divs.length > 0)
+      // get each messages id and set into an array
+      onReadMessage([...divs].filter(e => e.getAttribute('read') === "0").map(e => e.id));
+  }
+
+  return (
+    <div className="flex flex-grow relative overflow-hidden">
+      <div className="flex-col flex-grow w-full overflow-y-auto overflow-x-hidden"
+        ref={outerDiv}
+        onScroll={handleScroll}
+      >
+        <div
+          className="w-full relative transition-all duration-300 flex px-4 mb-4 flex-col-reverse mt-autos"
+          ref={innerDiv}
+          style={{ opacity: !isLoading ? 1 : 0}}
+        >
+          {children}
+        </div>
+
+        {/* Go to bottom button */}
+        <div className="absolute right-6 bottom-8"
+          style={{zIndex: showScrollButton ? 100 : -1000}}
+        >
+            <button className="relative p-2 w-fit h-fit bg-slate-200 rounded-full cursor-pointer shadow"
+                onClick={handleScrollButtonClick}
+                style={{ opacity: showScrollButton ? 1 : 0 }}
+            >
+                <span style={{ opacity: showNewMessages ? 1 : 0 }} className="text-xs rounded-full px-1 absolute -right-1 -top-1 text-white bg-emerald-600 h-fit w-fit">2</span>
+                <HiOutlineChevronDown className='w-6 h-6 text-slate-700' />
+            </button>
+        </div>
+        {
+          isLoading && 
+          <div className="absolute top-0 left-0 w-full h-full bg-transparent flex items-center justify-center text-2xl z-[1000] text-slate-600">
+            <div className="p-2">
+              <IoMdRefresh className="w-8 h-8 animate-spin" />
+            </div>
+          </div>
+        }
+      </div>
+    </div>
+  );
+};
+
+
+export default ScrollContainer;
